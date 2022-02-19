3e2a2a13c50ef0016ac2ba2e176bb78f671f2b43
Dmitry <dmikuivashev@gmail.com>
2022-02-19 18:09:01 +0300

    Initial commit

--- src/Performer.java
+++ src/Performer.java
@@ -2,31 +2,7 @@ import java.util.Scanner;
 
 
 public class Performer{
-    public static Set intersect(Set set1, Set set2){
-        Set set3 = new Set(new int[64]);
-        for(int i = 0; i < 64; i++){
-            if(set1.setarray[i] == set2.setarray[i] && set1.setarray[i] !=0){
-                set3.setarray[i] = 1;
-            }
-        }
-        return set3;
-    }
-    public static Set unit(Set set1, Set set2){
-        Set set3 = new Set(new int[64]);
-        for(int i = 0; i < 64; i++){
-            if(set1.setarray[i] == 1 || set2.setarray[i] == 1){
-                set3.setarray[i] = 1;
-            }
-        }
-        return set3;
-    }
-    public static String isInTheSet(Set set1, int x){
-        String flag = "no";
-        if(set1.setarray[x-1] == 1){
-            flag = "yes";
-        }
-        return flag;
-    }
+
     public static void main(String[] args){
         int n1, n2, x;
         Set set1 = new Set(new int[64]);
@@ -38,22 +14,22 @@ public class Performer{
         n2= scanner.nextInt();
         x = scanner.nextInt();
         for(int i = 0; i < n1; i++){
-            set1.setarray[scanner.nextInt()- 1] = 1;
+            set1.SetElements[scanner.nextInt()- 1] = 1;
         }
         for(int i = 0; i < n2; i++){
-            set2.setarray[scanner.nextInt()- 1] = 1;
+            set2.SetElements[scanner.nextInt()- 1] = 1;
         }
         for(int i = 0; i < 64; i++){
-            if(intersect(set1,set2).setarray[i] == 1)
+            if(Set.intersect(set1,set2).SetElements[i] == 1)
                 System.out.print(i + 1 + " ");
         }
         System.out.println();
         for(int i = 0; i < 64; i++){
-            if(unit(set1,set2).setarray[i] == 1)
+            if(Set.unit(set1,set2).SetElements[i] == 1)
                 System.out.print(i + 1 + " ");
         }
         System.out.println();
-        System.out.println(isInTheSet(set1, x));
+        System.out.println(Set.isInTheSet(set1, x));
     }
 }
 
--- src/Set.java
+++ src/Set.java
@@ -1,11 +1,35 @@
-import java.util.ArrayList;
+
 
 public class Set {
-    int numerator, denominator;
-    int[] setarray = new int[8];
-    public Set(int[] setarray){
-        this.setarray = setarray;
-    }
 
+    int[] SetElements;
+    public Set(int[] SetElements){
+        this.SetElements = SetElements;
+    }
+    public static Set intersect(Set set1, Set set2){
+        Set set3 = new Set(new int[64]);
+        for(int i = 0; i < 64; i++){
+            if(set1.SetElements[i] == set2.SetElements[i] && set1.SetElements[i] !=0){
+                set3.SetElements[i] = 1;
+            }
+        }
+        return set3;
+    }
+    public static Set unit(Set set1, Set set2){
+        Set set3 = new Set(new int[64]);
+        for(int i = 0; i < 64; i++){
+            if(set1.SetElements[i] == 1 || set2.SetElements[i] == 1){
+                set3.SetElements[i] = 1;
+            }
+        }
+        return set3;
+    }
+    public static String isInTheSet(Set set1, int x){
+        String flag = "no";
+        if(set1.SetElements[x-1] == 1){
+            flag = "yes";
+        }
+        return flag;
+    }
 
 }
