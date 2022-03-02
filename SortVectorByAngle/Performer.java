377323f3224164e2b1bb2dcc9dd246b6a003323c
rideau <93993119+adderallbaby@users.noreply.github.com>
2022-03-02 10:03:33 +0300

    Update Performer.java

--- SortVectorByAngle/Performer.java
+++ SortVectorByAngle/Performer.java
@@ -1,20 +1,22 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Performer {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n;
+
         n = scanner.nextInt();
         Vector[] listOfVectors = new Vector[n];
         for (int i = 0; i < n; i++) {
             double[] vectorCoordinates = new double[3];
             System.out.print("Enter vector coordinates {x,y,z}: ");
             for (int j = 0; j < 3; j++) {
-                vectorCoordinates[j] = scanner.nextInt();
+                vectorCoordinates[j] = scanner.nextDouble();
             }
             Vector currentVector = new Vector(vectorCoordinates);
             listOfVectors[i] = currentVector;
         }
-        Vector.sortByAngleToXOY(listOfVectors, n);
+        Arrays.sort(listOfVectors);
         for(int i = 0; i < n; i++){
             Vector.printCoordinatesString(listOfVectors[i]);
             System.out.println("\n");
